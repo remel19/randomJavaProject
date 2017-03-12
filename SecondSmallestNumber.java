e9182831726e440ce766b44912ed5cbac325acc3
Remel Kabir <remelkabir@gmail.com>
2017-03-12 16:16:21 -0400

    Finds the second smallest number given an Array

--- SecondSmallestNumber.java
+++ SecondSmallestNumber.java
@@ -1,30 +1,28 @@
-package x;
-
-
 public class SecondSmallestNumber {
-
-	public static void main(String[] args) {
-		//initial array
-		int[] inputArray = {42, 8, 13, 79, 5, 2, 18};
-		System.out.println(findSecondSmallest(inputArray));//prints the returned value to console
-		
-	}//main
-
-	//method to find the smallest value
-	public static int findSecondSmallest(int[] array){
-		int length = array.length;// length of the Array
-		
-		int smallest = array[0]; //assuming that is the smallest
-		int secondSmallest = 0; //yet to determine
-		
-		for(int i=0; i<length; i++){
-			if(smallest>array[i]){
-				secondSmallest = smallest;//setting the secondSmallest
-				smallest = array[i];
-			}
-		}
-		return secondSmallest;//return the value
-		
-	}//findSecondSmallest
-	
+    
+    public static void main(String[] args) {
+        //initial array
+        int[] inputArray = {42, 8, 13, 79, 2, 18, 1};
+        System.out.println(findSecondSmallest(inputArray));//prints the returned value to console
+        
+    }//main
+    
+    //method to find the smallest value
+    public static int findSecondSmallest(int[] array){
+        int length = array.length;// length of the Array
+        
+        int smallest = array[0];
+        int secondSmallest = smallest;
+        for(int i=0; i<length; i++){
+            if(array[i]<smallest){
+                secondSmallest = smallest;//setting the secondSmallest
+                smallest = array[i];
+            }
+            else if (array[i] < secondSmallest && array[i] != smallest)
+                secondSmallest = array[i];
+        }
+        return secondSmallest;//return the value
+        
+    }//findSecondSmallest
+    
 }//SecondSmallestNumber
